1f097643a0c5dd914750b805df86bbc46bc44259
Yusong Shi <jackshi.sys@gmail.com>
2022-03-09 09:33:24 -0500

    Basic functions working, need to figure out how to add score on last page

--- game.js
+++ game.js
@@ -1,34 +1,33 @@
-// Questions for the game
 let questions = [
     {
         question: "1. Whats Joey's special?",
         choices: [ "Bologna sandwich", "2 Pizzas", "Macaroni + Cheese", "Ramen"],
-        answer: 2,
+        answer: 1,
     },
     {   
         question: "2. How many times was Ross legally divorced?",
         choices: ["Twice", "Three times", "Four times", "Five times"],
-        answer: 2,
+        answer: 1,
     },
     {   
         question: "3. How many sisters does Joey have?",
         choices: ["2", "4", "7", "6"],
-        answer: 3,
+        answer: 2,
     },
     {   
         question: "4. Who did rachel discover her ex-fiancé Barry was cheating on her with?",
         choices: ["Her sister", "Her coworker", "Monica", "Her best friend"],
-        answer: 4,
+        answer: 3,
     },
     {   
         question: `5. Which of the following Chandler "facts" is true?`,
         choices: ["He has a third nipple", "He gas all his toes", "He dated Rachel", "He is a millionaire"],
-        answer: 1,
+        answer: 0,
     },
     {   
         question: `6. Which member of the Friends crew kissed Rachel’s mom?`,
         choices: ["Chandler", "Ross", "Joey", "Phoebe"],
-        answer: 3,
+        answer: 2,
     },
     {   
         question: "7. What’s Phoebe’s sister’s name?",
@@ -38,66 +37,167 @@ let questions = [
     {   
         question: "8. Who hates Thanksgiving?",
         choices: ["Monica", "Joey", "Chandler", "Ross"],
-        answer: 3,
+        answer: 2,
     },
 ]
 
 
-// console.log(questions[6])
 
 
 
-const answers = document.querySelectorAll(`.answer-text`)
-const question = document.querySelector(`#question`)
-const progress = document.querySelector(`#progress`)
-
-let questionCounter = 0
-let score = 0
-let correctAnswer = false
+let questionCount = 0;
+let score = 0;
+let correctAnswer = false;
 
 
 
+// grab html elements
+let choices = document.querySelectorAll('.answer-text');
+let question = document.querySelector('.question')
+let progress = document.querySelector(`#progress`)
+let currentScore = document.querySelector(`#score`)
+let finalScore = document.querySelector(`#end-game-text`)
 
-loadGame()
+
+window.onload = loadGame();
 
 
 function loadGame() {
-    question.innerHTML = questions[questionCounter].question
-    answers.forEach(function(choice, i) {
-        choice.innerHTML = questions[questionCounter].choices[i]
-    })
-    updateProgress()
+    question.innerText = questions[questionCount].question;
+    choices.forEach(function(choice, i) {
+        choice.innerText = questions[questionCount].choices[i];
+    });
+    
+    updateProgress();
 }
+
+choices.forEach(function(choice) {
+    choice.addEventListener('click', scoring);
+});
+
+
 
 function updateProgress() {
-    progress.innerHTML = `Question ${questionCounter+1} of 8`
+  progress.innerHTML = `Questions ${questionCount+1}/8`
 }
 
-answers.addEventListener( 'click', () => {
-    const question = questions[questionCounter].question
-    if(question.answer === answer.textContent){
-        score++
+function scoring() {
+    answer = questions[questionCount].answer;
+ 
+    if (this.innerText === questions[questionCount].choices[answer]) {
+        correctAnswer = true
+        score += 100
+        currentScore.innerText = score
+    } else {
+        correctAnswer = false;
     }
-    questionCounter++
-    getQuestion();
-})
+    nextQuestion();
+}
 
 
-
-
-function getQuestion () {
-    questionCounter++
-
-    if (count > 8) {
-        count = 8;
-    } else if (count !== 8) {
+function nextQuestion() {
+    questionCount++;
+    if (questionCount < 8) {
         loadGame();
-    } else if (count === 8) {
-        return window.location.assign('/end.html')
+    } else if (questionCount === 8) {
+        return window.location.assign('/endGame.html')
     }
+}
+
+
+
+function resetQuiz() {
     
-}
+    renderQuestion();
+}    
 
-function resetQuiz () {
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// choicesPara = document.getElementsByTagName('p')[1];
+
+// resetButton.addEventListener('click', resetQuiz);
+// let resetButton = document.getElementsByClassName('reset');
+// let prevButton = document.getElementsByClassName('prev');
+
+
+
+
+
+
+
+// the prevButton will only be available to go back one question
+// function prevQuestion() {
+    //     // when the previous question renders, remove the prevButton
+    //     prevFlag = false;
+    
+//     // if the user originally clicked the correctAnswer, remove score
+//     if (correctAnswer) {
+//         correctAnswer = false;
+//         score--;
+//     }
+    
+//     // then go back and render the old question
+//     count--;
+//     renderQuestion();
+// }
+
+
+
+
+
+// function renderCompletion() {
+//     updateProgress();
+    // scorePercentage = Math.round(score/20 * 100) + '%';
+    
+    // // update with a thank you note and the user's percentage
+    // question.innerText = 'Thank you for Completing the Quiz!';
+    // resultsPara.innerText = 'Your score is: ' + scorePercentage;
+    
+    // // reset avail, prevButton and choicesPara are removed
+    // choicesPara.classList.add('hide');
+    // prevButton.classList.add('hide');
+    // resetButton.classList.remove('hide');
+// }
+
+
+
+
+
+// function resetQuiz() {
+    // reset tracking variables
+    // count = 0;
+    // score = 0;
+    // correctAnswer = false;
+    // prevFlag = false;
+    
+    // // resultsPara is hidden
+    // resultsPara.innerText = '';
+    
+    // // choicesPara displays while resetButton is hidden
+    // choicesPara.classList.remove('hide');
+    // resetButton.classList.add('hide');
+    
+//     renderQuestion();
+// }    
+
+
+
+
+// function updateProgress() {
+        // progressPercentage = Math.round((count/20) * 100);
+        // progress.style.width = progressPercentage + '%';
+    // }
