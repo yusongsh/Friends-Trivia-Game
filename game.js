a84518927653345cbf68a63e688c2e2bb2f8e2dc
Yusong Shi <jackshi.sys@gmail.com>
2022-03-09 00:34:01 -0500

    basic functions working

--- game.js
+++ game.js
@@ -2,55 +2,43 @@
 let questions = [
     {
         question: "1. Whats Joey's special?",
-        answer1: [ "Bologna sandwich", "2 Pizzas", "Macaroni + Cheese", "Ramen"],
-        correctAnswer: 2,
+        choices: [ "Bologna sandwich", "2 Pizzas", "Macaroni + Cheese", "Ramen"],
+        answer: 2,
     },
     {   
         question: "2. How many times was Ross legally divorced?",
-        answer: ["Twice", "Three times", "Four times", "Five times"],
-        correctAnswer: 2,
+        choices: ["Twice", "Three times", "Four times", "Five times"],
+        answer: 2,
     },
     {   
         question: "3. How many sisters does Joey have?",
-        answer1: "2",
-        answer2: "4",
-        answer3: "7",
-        answer4: "6",
-        correctAnswer: 3,
+        choices: ["2", "4", "7", "6"],
+        answer: 3,
     },
     {   
         question: "4. Who did rachel discover her ex-fiancé Barry was cheating on her with?",
-        answer1: "Her sister",
-        answer2: "Her coworker",
-        answer3: "Monica",
-        answer4: "Her best friend",
-        correctAnswer: 4,
+        choices: ["Her sister", "Her coworker", "Monica", "Her best friend"],
+        answer: 4,
     },
     {   
         question: `5. Which of the following Chandler "facts" is true?`,
-        answer1: "He has a third nipple",
-        answer2: "He gas all his toes",
-        answer3: "He dated Rachel",
-        answer4: "He is a millionaire",
-        correctAnswer: 1,
+        choices: ["He has a third nipple", "He gas all his toes", "He dated Rachel", "He is a millionaire"],
+        answer: 1,
     },
     {   
         question: `6. Which member of the Friends crew kissed Rachel’s mom?`,
-        answer1: "Chandler",
-        answer2: "Ross",
-        answer3: "Joey",
-        answer4: "Phoebe",
-        correctAnswer: 3,
+        choices: ["Chandler", "Ross", "Joey", "Phoebe"],
+        answer: 3,
     },
     {   
         question: "7. What’s Phoebe’s sister’s name?",
-        answer: ["Amy", "Ursula", "Jill", "Monica"],
-        correctAnswer: 1,
+        choices: ["Amy", "Ursula", "Jill", "Monica"],
+        answer: 1,
     },
     {   
         question: "8. Who hates Thanksgiving?",
-        answer: ["Monica", "Joey", "Chandler", "Ross"],
-        correctAnswer: 3,
+        choices: ["Monica", "Joey", "Chandler", "Ross"],
+        answer: 3,
     },
 ]
 
@@ -69,11 +57,47 @@ let correctAnswer = false
 
 
 
-startGame()
 
-function startGame() {
+loadGame()
+
+
+function loadGame() {
     question.innerHTML = questions[questionCounter].question
-    answers.forEach(function(answer, i) {
-        answer.innerHTML = questions[questionCounter].answer[i]
+    answers.forEach(function(choice, i) {
+        choice.innerHTML = questions[questionCounter].choices[i]
     })
+    updateProgress()
+}
+
+function updateProgress() {
+    progress.innerHTML = `Question ${questionCounter+1} of 8`
+}
+
+answers.addEventListener( 'click', () => {
+    const question = questions[questionCounter].question
+    if(question.answer === answer.textContent){
+        score++
+    }
+    questionCounter++
+    getQuestion();
+})
+
+
+
+
+function getQuestion () {
+    questionCounter++
+
+    if (count > 8) {
+        count = 8;
+    } else if (count !== 8) {
+        loadGame();
+    } else if (count === 8) {
+        return window.location.assign('/end.html')
+    }
+    
+}
+
+function resetQuiz () {
+
 }
